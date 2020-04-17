7361f7a5724f795f16a79c422e187928e4c563ef
Mitch Villanueva <mitch@reign.com.au>
2020-04-17 13:08:39 +0800

    Test update

--- config/db.go
+++ config/db.go
@@ -1,35 +1,29 @@
 package config
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"log"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
-// 	"go.mongodb.org/mongo-driver/mongo"
-// 	"go.mongodb.org/mongo-driver/mongo/options"
-// )
+// Collection - Mongo DB collection global variable
+var Collection *mongo.Collection
 
-// var collection *mongo.Collection
+func init() {
 
-// func init() {
+	// connect to MongoDB
+	fmt.Println("Connecting to MongoDB")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	// connect to MongoDB
-// 	fmt.Println("Connecting to MongoDB")
-// 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	err = client.Connect(context.TODO())
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	collection = client.Database("allyshop").Collection("products")
-// }
-
-// TestPrint implements
-func TestPrint() {
-	fmt.Println("TestPrint")
+	Collection = client.Database("allyshop").Collection("products")
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import config "github.com/joycezemitchell/product-grpc-api/config"
+
 func main() {
 	config.TestPrint()
 }
